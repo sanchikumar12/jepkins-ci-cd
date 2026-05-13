b5810abbb776a6cc47a080560a7bcba68de5d02e
Sanchit Kumar <sanchitkumarsingh098@gmail.com>
2026-05-13 19:46:01 +0530

    Add Discovery-Server

---
+++ Discovery-Server/src/test/java/com/edulearn/learn/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edulearn.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
